9e66d26377a7221ea9b6463d511213155ca99b8b
Chris Glass <chris@exoscale.ch>
2018-05-08 10:29:34 +0200

    Fixed syntax, expanded specs.

--- src/raven/client.clj
+++ src/raven/client.clj
@@ -35,6 +35,7 @@
 
 (defn clear-http-info
   "reset this thread's http info."
+  []
   (swap! @thread-storage dissoc :request))
 
 (defn clear-context
@@ -294,13 +295,13 @@
    {:url url
     :method method})
   ([url method headers query_string cookies data env]
-    {:url url
-     :method method
-     :headers headers
-     :query_string query_string
-     :cookies cookies
-     :data data
-     :env env}))
+   {:url url
+    :method method
+    :headers headers
+    :query_string query_string
+    :cookies cookies
+    :data data
+    :env env}))
 
 (defn add-http-info!
   "Add HTTP information to the sentry context (or a thread-local storage)."
